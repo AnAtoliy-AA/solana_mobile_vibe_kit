85d40cae836bbbb792205da2551e407dca541a42
AnAtoliy-AA <aliaksandrau89@gmail.com>
2025-11-21 13:09:19 +0400

    fix: cursor issues

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'yarn start',
+    command: 'npm start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
